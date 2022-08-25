6bd36be23845f6baf94e79f6739acffc8f691e0e
sreyans002 <@anonymous56625>
2022-08-25 14:52:35 +0530

    minor

--- app/src/main/java/com/android/autelsdk/RemoteController/RemoteControllerActivity.kt
+++ app/src/main/java/com/android/autelsdk/RemoteController/RemoteControllerActivity.kt
@@ -1,12 +1,13 @@
 package com.android.autelsdk.RemoteController
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.android.autelsdk.R
 import com.android.autelsdk.databinding.ActivityRemoteControllerBinding
 
 class RemoteControllerActivity : AppCompatActivity() {
+    val TAG = RemoteControllerActivity::class.java.simpleName
     lateinit var binding : ActivityRemoteControllerBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {

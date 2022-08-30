7a824870776981c9283ab524747ea1e15ef61d55
DESTROYER\prudh <pnaidu@autelrobotics.com>
2022-08-30 11:28:42 +0530

    written

--- app/src/main/java/com/android/autelsdk/FlyController/FlyControllerViewModel.kt
+++ app/src/main/java/com/android/autelsdk/FlyController/FlyControllerViewModel.kt
@@ -1,5 +1,6 @@
 package com.android.autelsdk.FlyController
 
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.android.autelsdk.RemoteController.RemoteControllerRepository
@@ -10,4 +11,36 @@ class FlyControllerViewModel<AutelFlyController>(
     private val mController: com.autel.sdk.flycontroller.AutelFlyController,
     val flyControllerRepository : FlyControllerRepository
     ) : ViewModel(){
+
+    public fun setBeginnerModeStateTest (enable: Boolean) : MutableLiveData<Resource<String>> {
+        return flyControllerRepository.setBeginnerModeStateTest(enable)
+    }
+
+    public fun getBeginnerModeStateTest (view: View) : MutableLiveData<Resource<String>> {
+        return flyControllerRepository.getBeginnerModeStateTest(view)
+    }
+
+    public fun getMaxHeightTest (view: View) : MutableLiveData<Resource<String>> {
+        return flyControllerRepository.getMaxHeightTest(view)
+    }
+
+    public fun setMaxHeightTest (view: View) : MutableLiveData<Resource<String>> {
+        return flyControllerRepository.setMaxHeightTest(view)
+    }
+
+    public fun getMaxRangeTest (view: View) : MutableLiveData<Resource<String>> {
+        return flyControllerRepository.getMaxRangeTest(view)
+    }
+
+    public fun setMaxRangeTest (view: View) : MutableLiveData<Resource<String>> {
+        return flyControllerRepository.setMaxRangeTest(view)
+    }
+
+    public fun getReturnHeightTest (view: View) : MutableLiveData<Resource<String>> {
+        return flyControllerRepository.getReturnHeightTest(view)
+    }
+
+    public fun setReturnHeightTest (view: View) : MutableLiveData<Resource<String>> {
+        return flyControllerRepository.setReturnHeightTest(view)
+    }
 }

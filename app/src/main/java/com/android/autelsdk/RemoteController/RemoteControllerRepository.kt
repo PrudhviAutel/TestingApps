7c0443e49dbf6635c699f41dc3ba8e42566715d6
sreyans01 <sreyans.bohara@getkhabri.com>
2022-08-30 09:57:31 +0530

    added getter test cases of remote controller activity

--- app/src/main/java/com/android/autelsdk/RemoteController/RemoteControllerRepository.kt
+++ app/src/main/java/com/android/autelsdk/RemoteController/RemoteControllerRepository.kt
@@ -2,7 +2,7 @@ package com.android.autelsdk.RemoteController
 
 import androidx.lifecycle.MutableLiveData
 import com.android.autelsdk.util.Resource
-import com.autel.common.remotecontroller.RemoteControllerLanguage
+import com.autel.common.remotecontroller.*
 
 interface RemoteControllerRepository {
 
@@ -10,6 +10,33 @@ interface RemoteControllerRepository {
 
     fun getLanguageTest() : MutableLiveData<Resource<RemoteControllerLanguage>>
 
+    fun enterPairingTest() : MutableLiveData<Resource<String>>
 
+    //TODO : How to test exit Pairing method as it has no Call back element
 
+    fun setRFPowerTest(rfPower: RFPower) : MutableLiveData<Resource<String>>
+
+    fun getRFPowerTest() : MutableLiveData<Resource<RFPower>>
+
+    fun setTeacherStudentModeTest(teachingMode: TeachingMode) : MutableLiveData<Resource<String>>
+
+    fun getTeacherStudentModeTest() : MutableLiveData<Resource<TeachingMode>>
+
+    //TODO : How to test startCalibration and saveCalibration
+
+    fun setParameterUnitTest(parameterUnit : RemoteControllerParameterUnit) : MutableLiveData<Resource<String>>
+
+    fun getParameterUnitTest() : MutableLiveData<Resource<RemoteControllerParameterUnit>>
+
+    fun setRCCommandStickModeTest(commandStickMode: RemoteControllerCommandStickMode) : MutableLiveData<Resource<String>>
+
+    fun getRCCommandStickModeTest() : MutableLiveData<Resource<RemoteControllerCommandStickMode>>
+
+    fun setYawCoefficientTest(yawCoeff : Float) : MutableLiveData<Resource<String>>
+
+    fun getYawCoefficientTest() : MutableLiveData<Resource<Float>>
+
+    fun getVersionInfoTest() : MutableLiveData<Resource<RemoteControllerVersionInfo>>
+
+    fun getSerialNumberTest() : MutableLiveData<Resource<String>>
 }
--- app/src/main/java/com/android/autelsdk/RemoteController/RemoteControllerRepositoryImpl.kt
+++ app/src/main/java/com/android/autelsdk/RemoteController/RemoteControllerRepositoryImpl.kt
@@ -6,7 +6,7 @@ import com.android.autelsdk.util.Resource
 import com.autel.common.CallbackWithNoParam
 import com.autel.common.CallbackWithOneParam
 import com.autel.common.error.AutelError
-import com.autel.common.remotecontroller.RemoteControllerLanguage
+import com.autel.common.remotecontroller.*
 
 class RemoteControllerRepositoryImpl<AutelRemoteController>(
     private val mController: com.autel.sdk.remotecontroller.AutelRemoteController,
@@ -43,7 +43,146 @@ class RemoteControllerRepositoryImpl<AutelRemoteController>(
         return getLanguageTestResult
     }
 
+    override fun enterPairingTest(): MutableLiveData<Resource<String>> {
+        var enterPairingTestResult : MutableLiveData<Resource<String>> = MutableLiveData()
+        mController.enterPairing(object : CallbackWithNoParam {
+            override fun onFailure(rcError: AutelError) {
+                val errorMessage = "";
+                enterPairingTestResult.postValue(Resource.Companion.error(errorMessage, null))
+            }
 
+            override fun onSuccess() {
+                val successMessage = "";
+                enterPairingTestResult.postValue(Resource.Companion.success(successMessage))
+            }
+        })
+        return enterPairingTestResult
+    }
+
+    override fun setRFPowerTest(rfPower: RFPower): MutableLiveData<Resource<String>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getRFPowerTest(): MutableLiveData<Resource<RFPower>> {
+        var getRFPowerTestResult : MutableLiveData<Resource<RFPower>> = MutableLiveData()
+        mController.getRFPower(object : CallbackWithOneParam<RFPower> {
+            override fun onFailure(rcError: AutelError) {
+                val errorMessage = "";
+                getRFPowerTestResult.postValue(Resource.Companion.error(errorMessage, null))
+            }
+
+            override fun onSuccess(rfPower: RFPower?) {
+                getRFPowerTestResult.postValue(Resource.Companion.success(rfPower))
+            }
+        })
+        return getRFPowerTestResult
+    }
+
+    override fun setTeacherStudentModeTest(teachingMode: TeachingMode): MutableLiveData<Resource<String>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTeacherStudentModeTest(): MutableLiveData<Resource<TeachingMode>> {
+        var getTeacherStudentModeTestResult : MutableLiveData<Resource<TeachingMode>> = MutableLiveData()
+        mController.getTeachingMode(object : CallbackWithOneParam<TeachingMode> {
+            override fun onFailure(rcError: AutelError) {
+                val errorMessage = "";
+                getTeacherStudentModeTestResult.postValue(Resource.Companion.error(errorMessage, null))
+            }
+
+            override fun onSuccess(teachingMode: TeachingMode?) {
+                getTeacherStudentModeTestResult.postValue(Resource.Companion.success(teachingMode))
+            }
+        })
+        return getTeacherStudentModeTestResult
+    }
+
+    override fun setParameterUnitTest(parameterUnit: RemoteControllerParameterUnit): MutableLiveData<Resource<String>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getParameterUnitTest(): MutableLiveData<Resource<RemoteControllerParameterUnit>> {
+        var getParameterUnitTestResult : MutableLiveData<Resource<RemoteControllerParameterUnit>> = MutableLiveData()
+        mController.getLengthUnit(object : CallbackWithOneParam<RemoteControllerParameterUnit> {
+            override fun onFailure(rcError: AutelError) {
+                val errorMessage = "";
+                getParameterUnitTestResult.postValue(Resource.Companion.error(errorMessage, null))
+            }
+
+            override fun onSuccess(parameterUnit: RemoteControllerParameterUnit?) {
+                getParameterUnitTestResult.postValue(Resource.Companion.success(parameterUnit))
+            }
+        })
+        return getParameterUnitTestResult
+    }
+
+    override fun setRCCommandStickModeTest(commandStickMode: RemoteControllerCommandStickMode): MutableLiveData<Resource<String>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getRCCommandStickModeTest(): MutableLiveData<Resource<RemoteControllerCommandStickMode>> {
+        var getRCCommandStickModeTestResult : MutableLiveData<Resource<RemoteControllerCommandStickMode>> = MutableLiveData()
+        mController.getCommandStickMode(object : CallbackWithOneParam<RemoteControllerCommandStickMode> {
+            override fun onFailure(rcError: AutelError) {
+                val errorMessage = "";
+                getRCCommandStickModeTestResult.postValue(Resource.Companion.error(errorMessage, null))
+            }
+
+            override fun onSuccess(commandStickMode: RemoteControllerCommandStickMode?) {
+                getRCCommandStickModeTestResult.postValue(Resource.Companion.success(commandStickMode))
+            }
+        })
+        return getRCCommandStickModeTestResult
+    }
+
+    override fun setYawCoefficientTest(yawCoeff: Float): MutableLiveData<Resource<String>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getYawCoefficientTest(): MutableLiveData<Resource<Float>> {
+        var getYawCoefficientTestResult : MutableLiveData<Resource<Float>> = MutableLiveData()
+        mController.getYawCoefficient(object : CallbackWithOneParam<Float> {
+            override fun onFailure(rcError: AutelError) {
+                val errorMessage = "";
+                getYawCoefficientTestResult.postValue(Resource.Companion.error(errorMessage, null))
+            }
+
+            override fun onSuccess(yawCoeff: Float?) {
+                getYawCoefficientTestResult.postValue(Resource.Companion.success(yawCoeff))
+            }
+        })
+        return getYawCoefficientTestResult
+    }
+
+    override fun getVersionInfoTest(): MutableLiveData<Resource<RemoteControllerVersionInfo>> {
+        var getVersionInfoTestResult : MutableLiveData<Resource<RemoteControllerVersionInfo>> = MutableLiveData()
+        mController.getVersionInfo(object : CallbackWithOneParam<RemoteControllerVersionInfo> {
+            override fun onFailure(rcError: AutelError) {
+                val errorMessage = "";
+                getVersionInfoTestResult.postValue(Resource.Companion.error(errorMessage, null))
+            }
+
+            override fun onSuccess(versionInfo: RemoteControllerVersionInfo?) {
+                getVersionInfoTestResult.postValue(Resource.Companion.success(versionInfo))
+            }
+        })
+        return getVersionInfoTestResult
+    }
+
+    override fun getSerialNumberTest(): MutableLiveData<Resource<String>> {
+        var getSerialNumberTestResult : MutableLiveData<Resource<String>> = MutableLiveData()
+        mController.getSerialNumber(object : CallbackWithOneParam<String> {
+            override fun onFailure(rcError: AutelError) {
+                val errorMessage = "";
+                getSerialNumberTestResult.postValue(Resource.Companion.error(errorMessage, null))
+            }
+
+            override fun onSuccess(serialNumber: String?) {
+                getSerialNumberTestResult.postValue(Resource.Companion.success(serialNumber))
+            }
+        })
+        return getSerialNumberTestResult
+    }
 
 
 }

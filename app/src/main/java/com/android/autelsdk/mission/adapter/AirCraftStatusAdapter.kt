4c3147102f7e0fbf2fe766068184cc659c63c698
rahul <rkalamkar@autelrobotics.com>
2022-09-29 15:08:45 +0530

    Fragments for mission module added

---
+++ app/src/main/java/com/android/autelsdk/mission/adapter/AirCraftStatusAdapter.kt
@@ -0,0 +1,39 @@
+package com.android.autelsdk.mission.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.autelsdk.databinding.SingleTextitemBinding
+import com.android.autelsdk.util.GeneralUtils
+
+class AirCraftStatusAdapter : RecyclerView.Adapter<AirCraftStatusViewHolder>() {
+    val moduleList = ArrayList<String>()
+    lateinit var context: Context
+
+    init {
+        moduleList.addAll(GeneralUtils.getACStatusCommandForBattery())
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AirCraftStatusViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = SingleTextitemBinding.inflate(inflater, parent, false)
+        context = parent.context
+        return AirCraftStatusViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AirCraftStatusViewHolder, position: Int) {
+        val item = moduleList[position]
+        holder.binding.getButton.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return moduleList.size
+    }
+}
+
+
+class AirCraftStatusViewHolder(val binding: SingleTextitemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+}
+
